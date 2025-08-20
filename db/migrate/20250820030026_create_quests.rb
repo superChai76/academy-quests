d1cf355b342a381e4e8fac8b5d8c78320bc48e0a
PL <supsupchi@gmail.com>
2025-08-20 16:29:03 +0700

    feat: Add dafault to is_done

--- db/migrate/20250820030026_create_quests.rb
+++ db/migrate/20250820030026_create_quests.rb
@@ -2,7 +2,7 @@ class CreateQuests < ActiveRecord::Migration[8.0]
   def change
     create_table :quests do |t|
       t.text :description
-      t.boolean :is_done
+      t.boolean :is_done, default: false
 
       t.timestamps
     end
